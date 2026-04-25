49a0383fd4ad92f4e979189fc66a5a7ed4716793
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-04-24 22:31:50 -0300

    comienza el proyecto

---
+++ eslint.config.ts
@@ -0,0 +1,25 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      'no-empty-pattern': 'off',
+      '@stylistic/quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'space-before-function-paren': ['error', 'always'],
+      '@stylistic/arrow-parens': ['error', 'always']
+    }
+  }
+])
