c1cdbec2fc3bd2629c16b90caf96072cd7a69fcb
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-04-24 22:35:57 -0300

    feat: añade express y endpoint de prueba

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Hello via Bun!')
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT ?? 1234
+
+app.listen(port, () => {
+  console.log('Escuchando en el puerto:', port)
+})
